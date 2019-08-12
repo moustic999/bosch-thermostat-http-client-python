bf2beec6f04cccac946a188d84ebabb96703adb5
Pawel Szafer <pszafer@gmail.com>
2019-08-12 15:04:40 +0200

    rename db_prototype to db

--- bosch_thermostat_http/db.py
+++ bosch_thermostat_http/db.py
@@ -2,12 +2,13 @@
 import json
 import os
 
-from .const import (PATHS, GATEWAY, DICT, GATEWAY, HEATING_CIRCUITS, DHW_CIRCUITS,
-    MANUAL, AUTO, MAX, MIN, UNITS, STATE, INVALID, VALUE, ALLOWED_VALUES, OWNPROGRAM, HCPROGRAM, OPEN, SHORT,
-    UUID, FIRMWARE_VERSION, HARDWARE_VERSION, SYSTEM_BRAND, SYSTEM_TYPE,
-    MAIN_URI, REFS)
+from .const import (PATHS, GATEWAY, DICT, GATEWAY, HEATING_CIRCUITS,
+                    DHW_CIRCUITS, MANUAL, AUTO, MAX, MIN, UNITS, STATE,
+                    INVALID, VALUE, ALLOWED_VALUES, OWNPROGRAM, HCPROGRAM,
+                    OPEN, SHORT, UUID, FIRMWARE_VERSION, HARDWARE_VERSION,
+                    SYSTEM_BRAND, SYSTEM_TYPE, MAIN_URI, REFS)
 
-FILENAME = os.path.join(os.path.dirname(__file__), 'db2_prototype.json')
+FILENAME = os.path.join(os.path.dirname(__file__), 'db.json')
 
 DATA = "data"
 FIRMWARE_URI = "versionFirmwarePath"
@@ -42,9 +43,15 @@ def check_db(fw_version, db):
     if fw_version in db:
         subrow = db[fw_version]
         if DICT in subrow and GATEWAY in subrow:
-            if not all(k in subrow[DICT] for k in (MANUAL, AUTO, MAX, MIN, UNITS, STATE, INVALID, VALUE, ALLOWED_VALUES, OWNPROGRAM, HCPROGRAM, OPEN, SHORT)):
+            if not all(k in subrow[DICT] for k in (MANUAL, AUTO, MAX, MIN,
+                                                   UNITS, STATE, INVALID,
+                                                   VALUE, ALLOWED_VALUES,
+                                                   OWNPROGRAM, HCPROGRAM,
+                                                   OPEN, SHORT)):
                 return False
-            if not all(k in subrow[DICT] for k in (UUID, FIRMWARE_VERSION, HARDWARE_VERSION, SYSTEM_BRAND, SYSTEM_TYPE)):
+            if not all(k in subrow[DICT] for k in (UUID, FIRMWARE_VERSION,
+                                                   HARDWARE_VERSION,
+                                                   SYSTEM_BRAND, SYSTEM_TYPE)):
                 return False
         else:
             return False
